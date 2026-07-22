c857cbfa5bfe7e149eb858f83f1901e54b323b2c
David <132082715+ItzDavidWasTaken@users.noreply.github.com>
2026-07-22 15:42:10 +0000

    Optimising Inputs

--- controller.js
+++ controller.js
@@ -1,121 +1,234 @@
 module.exports = function(win){
 
-
-    console.log(
-        "Controller started"
-    );
+    console.log("Controller started");
 
 
     win.webContents.executeJavaScript(`
 
-
-        (()=>{
-
-
-            let previous = [];
+    (()=>{
 
 
-            function press(action){
+        let previousButtons = [];
 
-                window.controllerAPI.sendAction(
-                    action
-                );
+        let lastMove = 0;
+
+        const MOVE_DELAY = 180;
+
+
+        function press(action){
+
+            window.controllerAPI.sendAction(action);
+
+        }
+
+
+
+        function buttonPressed(index){
+
+            const pads =
+                navigator.getGamepads();
+
+
+            const pad =
+                pads[0];
+
+
+            if(!pad)
+                return false;
+
+
+            return pad.buttons[index]?.pressed;
+
+        }
+
+
+
+        function handleButton(index, action){
+
+
+            if(
+                buttonPressed(index) &&
+                !previousButtons[index]
+            ){
+
+                press(action);
+
+            }
+
+
+        }
+
+
+
+        function handleStick(){
+
+
+            const pads =
+                navigator.getGamepads();
+
+
+            const pad =
+                pads[0];
+
+
+            if(!pad)
+                return;
+
+
+            const now =
+                Date.now();
+
+
+            if(
+                now - lastMove < MOVE_DELAY
+            )
+                return;
+
+
+
+            const DEADZONE = 0.65;
+
+
+            let x =
+                pad.axes[0];
+
+
+            let y =
+                pad.axes[1];
+
+
+
+            if(Math.abs(x) > DEADZONE){
+
+
+                lastMove = now;
+
+
+                if(x < 0)
+                    press("LEFT");
+                else
+                    press("RIGHT");
+
+
+                return;
 
             }
 
 
 
-            function poll(){
+            if(Math.abs(y) > DEADZONE){
 
 
-                let pads =
-                    navigator.getGamepads();
+                lastMove = now;
 
 
-                let pad =
-                    pads[0];
+                if(y < 0)
+                    press("UP");
+                else
+                    press("DOWN");
 
 
-                if(pad){
+            }
 
 
-                    pad.buttons.forEach(
-                        (button,index)=>{
+        }
 
 
-                            if(
-                                button.pressed &&
-                                !previous[index]
-                            ){
 
 
-                                switch(index){
+
+        function poll(){
 
 
-                                    case 0:
-                                        press("SELECT");
-                                        break;
+            const pads =
+                navigator.getGamepads();
 
 
-                                    case 1:
-                                        press("BACK");
-                                        break;
+            const pad =
+                pads[0];
 
 
-                                    case 9:
-                                        press("PLAY");
-                                        break;
+
+            if(pad){
 
 
-                                }
+
+                // A
+                handleButton(
+                    0,
+                    "SELECT"
+                );
 
 
-                            }
+                // B
+                handleButton(
+                    1,
+                    "BACK"
+                );
 
 
-                            previous[index] =
-                                button.pressed;
+                // Start
+                handleButton(
+                    9,
+                    "PLAY"
+                );
 
 
-                        }
+
+                // D-pad
+                handleButton(
+                    12,
+                    "UP"
+                );
+
+
+                handleButton(
+                    13,
+                    "DOWN"
+                );
+
+
+                handleButton(
+                    14,
+                    "LEFT"
+                );
+
+
+                handleButton(
+                    15,
+                    "RIGHT"
+                );
+
+
+
+                previousButtons =
+                    pad.buttons.map(
+                        b=>b.pressed
                     );
 
 
-                    let x =
-                        pad.axes[0];
 
-                    let y =
-                        pad.axes[1];
-
-
-                    if(x < -0.8)
-                        press("LEFT");
-
-                    if(x > 0.8)
-                        press("RIGHT");
-
-                    if(y < -0.8)
-                        press("UP");
-
-                    if(y > 0.8)
-                        press("DOWN");
-
-
-                }
-
-
-                requestAnimationFrame(
-                    poll
-                );
+                handleStick();
 
 
             }
 
 
-            poll();
+
+            requestAnimationFrame(
+                poll
+            );
 
 
-        })();
+        }
+
+
+
+        poll();
+
+
+
+    })();
 
 
     `);
